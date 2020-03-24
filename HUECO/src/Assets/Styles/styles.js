f2ac2b201e61295b8dee69447a7ac0ba8a43fd64
johnsalzy <salzyjohn@gmail.com>
2020-03-24 19:06:44 -0400

    Update login page and styling

--- HUECO/src/Assets/Styles/styles.js
+++ HUECO/src/Assets/Styles/styles.js
@@ -2,31 +2,44 @@ import { StyleSheet } from 'react-native'
 
 const buttons = StyleSheet.create({
     primary: {
-        flex: 1,
-        height: 70,
-        backgroundColor: 'red',
-        justifyContent: 'center',
+        height: 50,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+        backgroundColor: '#E0F2E9',
+        borderColor: '#4F6E91',
+        color: 'black',
+        borderRadius: 30,
+        borderWidth: 3, 
+        width: '80%',
+        marginBottom: '1%',
+        marginTop: '2%',
         alignItems: 'center',
-        marginLeft: 20,
-        marginRight: 20
+        textAlignVertical: 'center',
+        alignContent: 'center'
+
     }
 })
 const text_input = StyleSheet.create({
     default: {
-        height: 40,
-        backgroundColor: 'white',
+        height: 50,
+        backgroundColor: '#4F6E91',
+        borderColor: '#E0F2E9',
         marginTop: 20,
         paddingLeft: 15,
-        width: '70%',
-        borderRadius: 20,
-        borderWidth: 1, 
-        borderColor: '#D84727',
+        width: '80%',
+        borderRadius: 50,
+        borderWidth: 2, 
+        color: '#E0F2E9',
+        
+        
     }
 })
 
 const view_style = StyleSheet.create({
-    default: {
-        alignItems: 'center'
+    center: {
+        alignItems: 'center',
+        alignContent: 'center'
     }
 })
   
--- HUECO/src/Screens/login.js
+++ HUECO/src/Screens/login.js
@@ -1,10 +1,10 @@
 // Import needed Libs
 import React, { Component } from "react";
+import { Button } from 'react-native-elements';
 import {
     View,
     Text,
     StyleSheet,
-    Button,
     TextInput,
     TouchableOpacity,
 } from "react-native";
@@ -17,15 +17,36 @@ class Login extends Component {
         username: '',
         password: '',
         loggedIn: false,
+        errorPresent: false,
+        error_text: '',
+        loginSuccess: false,
+        registerUser: false
+    }
+    login_username = (user, pass) => {
+        this.setState({errorPresent: false, error_text: ""})
+        alert('Logging in user')
+        if (this.state.username.length===0 || this.state.password.length===0 ){
+            this.setState({errorPresent: true, error_text: "Please Enter a Username/Password"})
+            return
+        } 
+        //Call API to validate login
+        if(this.state.loginSuccess) {
+            this.setState({errorPresent: true, error_text: "Invalid Login"})
+            return
+        } else {
+            this.setState({loginSuccess: true})
+        }
+
+        this.setState({errorPresent: true, error_text: 'Invalid Login'})
     }
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.container}>
-                    <Text style={{color:'#EF7B45', fontSize:70, textAlign: 'center', fontWeight: 'bold'}}>
-                      Heuco
+                    <Text style={{color:'#F4DF73', paddingBottom: '10%', fontSize:70, textAlign: 'center', fontWeight: 'bold'}}>
+                      Hueco
                     </Text>
-                    <View style={view_style.default}>
+                    <View style={view_style.center}>
                         <TextInput style={text_input.default} 
                             placeholder='Username'
                             onChangeText = {(username) => this.setState({username})}
@@ -38,10 +59,33 @@ class Login extends Component {
                         />
                     </View>
                     <TouchableOpacity onPress={() => alert('Reset Password Page Here')}>
-                        <Text style={{color: 'white', fontSize:10, paddingTop: '5%', 
+                        <Text style={{color: 'white', fontSize:10, paddingTop: '3%', paddingBottom: '2%',
                                         paddingLeft: '5%', textAlign: 'center'}}
+                        >Forgot Password?
+                        </Text>
+                    </TouchableOpacity>
+
+                    {/* To Display Login Errors */}
+                    <Text style={{color: 'red', textAlign: 'center'}}>
+                        {this.state.errorPresent ? this.state.error_text: ""}
+                    </Text>
+
+                    <TouchableOpacity style={view_style.center } onPress={()=> this.login_username(this.state.user, this.state.pass)}>
+                        <Text style={buttons.primary}
                         >
-                        Forgot Password?
+                            Login
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={ view_style.center} onPress={() => alert('Logging In with FB/Google')}>
+                        <Text style={buttons.primary}
+                        >
+                            Login With Google or Facebook
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={ view_style.center} onPress={() => alert('Register')}>
+                        <Text style={buttons.primary}
+                        >
+                            Register
                         </Text>
                     </TouchableOpacity>
                 </View>
@@ -54,7 +98,7 @@ export default Login;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#042A2B',
+        backgroundColor: '#052F5F',
         paddingTop: 40
     }
 });
