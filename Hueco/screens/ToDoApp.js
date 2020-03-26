625b5e01dab5cb87dfa07ac6b3a8f142a33c9417
johnsalzy <salzyjohn@gmail.com>
2020-03-26 11:04:36 -0400

    Add Redux and todo list app screen

---
+++ Hueco/screens/ToDoApp.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
+
+//Import Component/Store/etc.
+import AddTodo from '../redux/containers/AddTodo'
+import VisableTodos from '../redux/containers/VisableTodos'
+
+class TodoApp extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <AddTodo />
+                <View>
+                    
+                    <Text style={{fontSize:20, paddingTop: '5%', paddingLeft: '5%', alignContent: 'center', fontWeight: 'bold'}}>
+                      Dispaly List
+                    </Text>
+                  <VisableTodos />
+                </View>
+            </View>
+        );
+    }
+}
+export default TodoApp;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 40
+    }
+});
