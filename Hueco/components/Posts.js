1a50ecf915760bb68bd7d8cab561ed39a04fa5b1
johnsalzy <salzyjohn@gmail.com>
2020-03-31 18:18:45 -0400

    Update Posts.js
    Update to pull some real data (not filler data)

--- Hueco/components/Posts.js
+++ Hueco/components/Posts.js
@@ -30,16 +30,43 @@ class Posts extends Component {
             username: this.props.login.username,
             access_token: this.props.login.access_token,
             userData: this.props.user,
+            baseAPI: "http://3.133.123.120:8000/api/v1/",
             postData: [],
-            loadedPosts: 5
+            loadedPosts: 5,
+
+            // For New API call
+            dataFetched: false,
+            fetchData: {count: 0, results: []},
+            prevData: false, 
+            nextData: false,
         }
     }
     async componentDidMount(){
-        const response = await fetch(`https://jsonplaceholder.typicode.com/photos`)
+        let {baseAPI, access_token} = this.state;
+        const response = await fetch('https://jsonplaceholder.typicode.com/photos')
         const json = await response.json();
         this.setState({ postData: json});
-        
+        this.loadPostData(baseAPI + 'feed', access_token)
     }
+    async loadPostData(apiPath, access_token){
+        this.setState({dataFetched: false, prevData: false, nextData: false})
+        try {
+          //Assign the promise unresolved first then get the data using the json method.
+          const response = await fetch(apiPath, {
+            headers: {
+                'Authorization': 'Bearer ' + access_token,
+            }
+          })
+          .then((response) => response.json())
+          .then((responseData) => {
+              this.setState({dataFetched: true, fetchData: responseData, prevData: responseData.previous, nextData: responseData.next})
+          })
+          .done();
+        } catch(err) {
+            alert("Error with data -----------" + err);
+        }
+    }
+    
     viewPhoto = (id) => {
         alert('Viewing photo details photo: ' + id)
     }
@@ -58,10 +85,12 @@ class Posts extends Component {
         let totalPosts = this.state.postData.length
         let loadedPosts = this.state.loadedPosts
         let postData = this.state.postData.slice(2, loadedPosts)
+        let {fetchData} = this.state;
         return (
             <View style={styles.container}>
-                {postData.length == 0 ? <Text>No Posts ):</Text> : 
-                    postData.map((data, index) => (
+                {/* <Text>Post Dat: {JSON.stringify(fetchData.results[0])}</Text> */}
+                {fetchData.length == 0 ? <Text>No Posts ):</Text> : 
+                    fetchData.results.map((data, index) => (
                     <View style={{
                         borderColor: 'black', borderWidth: 3, 
                         marginTop: 10, 
@@ -71,7 +100,7 @@ class Posts extends Component {
                         }} 
                         key={index}>
                         <Image 
-                            source={{'uri': data.url}}  
+                            source={{'uri': data.media.media}}  
                             style={{    
                                 alignSelf: 'center',
                                 height: 400,
@@ -85,9 +114,9 @@ class Posts extends Component {
                                     <View style={{flexWrap: 'wrap', flexDirection: 'row'}}>
                                         {/* This is the thumbnail section */}
                                         <View style={{justifyContent: 'center', paddingRight: 10}}>
-                                            <TouchableOpacity  onPress={() => this.visitProfile(data.id)}>
+                                            <TouchableOpacity  onPress={() => this.visitProfile(data.user.id)}>
                                                 <Image 
-                                                    source={{'uri': this.state.userData.profile_pic}}  
+                                                    source={{'uri': data.media.media}}  
                                                     style={{    
                                                         height: 50,
                                                         width: 50,
@@ -100,17 +129,17 @@ class Posts extends Component {
                                         </View>
                                         {/* This is the Title section */}
                                         <View style= {{alignItems: 'center', justifyContent: 'center', width: '65%'}}>
-                                            <Text style={{fontWeight: 'bold', fontSize: 15, textAlign: 'center',}}>{data.title}</Text>
+                                            <Text style={{fontWeight: 'bold', fontSize: 15, textAlign: 'center',}}>{data.comments}</Text>
                                         </View>
                                         {/* This is likes/comments section */}
                                         <View style={{marginLeft: 'auto'}}>
                                             <TouchableOpacity onPress={() => this.likePhoto(data.id)} style={{flexDirection: 'row', padding: 1, alignItems: 'center', paddingLeft: 10}}>
-                                                <TabBarIcon color='red' name={'md-heart'}/><Text style={{ paddingLeft: 5, color: 'red', fontSize: 15}}>{data.id}</Text>
+                                                <TabBarIcon color='red' name={'md-heart'}/><Text style={{ paddingLeft: 5, color: 'red', fontSize: 15}}>{data.likes}</Text>
                                             </TouchableOpacity>
 
                                             <TouchableOpacity onPress={() => this.viewPhoto(data.id)} style={{flexDirection: 'row', padding: 1, alignItems: 'center', paddingLeft: 10}}>
                                                 <TabBarIcon color='#b863c7' name={'ios-text'}/>
-                                                <Text style={{paddingLeft: 5, color: '#b863c7', fontSize: 15}}>{data.id-1}</Text>
+                                                <Text style={{paddingLeft: 5, color: '#b863c7', fontSize: 15}}>{data.stars}</Text>
                                             </TouchableOpacity>
                                         </View>
                                     </View>
