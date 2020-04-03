84d116653536b2e3c51b10b38171cf2f6fad6568
johnsalzy <salzyjohn@gmail.com>
2020-04-02 23:08:09 -0400

    Update AboutUs.js
    Update styling, text, data

--- Hueco/screens/AboutUs.js
+++ Hueco/screens/AboutUs.js
@@ -1,123 +1,51 @@
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Divider } from 'react-native-elements'
 import { ScrollView } from 'react-native-gesture-handler';
-import * as WebBrowser from 'expo-web-browser';
 
-import { MonoText } from '../components/StyledText';
+import { app_styles } from '../assets/styles/universal';
 
 export default function HomeScreen() {
   return (
+    <View style={app_styles.screen}>
       <ScrollView>
-          <View style={{paddingTop: 10, alignItems: 'center'}}>
-            <Text>Welcome to Heuco!!</Text>
+        <Text style={styles.welcomeText}>Welcome to Heuco!!</Text>
 
-            <Text>Heuco is .........!!</Text>
-            <Text>Hello!!</Text>
-            <Text>Hello!!</Text>
-            <Text>Hello!!</Text>
-            <Text>Hello!!</Text>
-            <Text>Hello!!</Text>
-            <Text>Hello!!</Text>
-          </View>
+        <View style={{paddingTop: 10}}>
+          <Text style={styles.bigText}>Our Goal</Text>
+          <Divider style={styles.divider} />
+          <Text style={styles.goalText}>
+            {"\t"}Web services e-markets, transition reinvent engage strategize redefine. 
+            Social group buying crowded market freemium prototype Early stage disruptive ecosystem community outreach dynamic location based strategic investor. 
+            Big data infographic. Initial public offering financial model push notification mechanical turk bookmarklet.
+            {"\n"}{"\n"}
+            {"\t"}Coworking viral landing page user base minimum viable product hackathon API mashup FB Connect. 
+            Evolve metrics innovative iterate: peer-to-peer communities communities robust, proactive, 
+            web-readiness markets infomediaries platforms 24/365 innovate synergize deliverables cross-platform.
+          </Text>
+        </View>
+
+        <Text style={styles.bigText}>Advangtages of Heuco - Users</Text>
+        <Divider style={styles.divider} />
+        <Text>{'\u2022'} Aggressive workouts to help you train</Text>
+        <Text>{'\u2022'} Compatible for indoor & outdoor routes</Text>
+        <Text>{'\u2022'} Route & grade tracking to help visualize progress</Text>
+        <Text>{'\u2022'} Watch & cheer on friends as they complete routes</Text>
+
+        <Text style={styles.bigText}>Advangtages of Heuco - Gyms</Text>
+        <Divider style={styles.divider} />
+        <Text>{'\u2022'} See what routes are/arent being climbed</Text>
+        <Text>{'\u2022'} Display to users new routes that your gym has</Text>
+
       </ScrollView>
-
+    </View>
 
   );
 }
 
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-
-
-
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+    divider: { marginTop: 5, backgroundColor: '#052F5F', height: 2 },
+    welcomeText: {fontSize: 25, fontWeight: 'bold', textAlign: 'center', color: '#052F5F'},
+    bigText: {paddingTop: 10, fontSize: 19, fontWeight: 'bold', textAlign: 'center', color: '#052F5F'},
+    goalText: {textAlign: 'center'},
 });
