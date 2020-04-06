17036c6d3d5c0808c1524db5d748f1ce48e6ccf8
johnsalzy <salzyjohn@gmail.com>
2020-04-06 14:12:02 -0400

    Update Profile.js
    make data load every time, not fetch from parent comp

--- Hueco/components/User/Profile.js
+++ Hueco/components/User/Profile.js
@@ -7,6 +7,7 @@ import {
     TouchableOpacity,
     Image,
     Dimensions,
+    ActivityIndicator,
 } from "react-native";
 import {connect} from 'react-redux';
 import { Tooltip } from 'react-native-elements';
@@ -29,21 +30,52 @@ class Profile extends Component {
         this.state = {
             login: this.props.login,
             data: this.props.data,
+            id: this.props.data.id,
             myProfile: false,
+            baseAPI: 'http://3.133.123.120:8000/api/v1/',
+            userData: null,
+            is_following: null,
+            profileDataLoaded: false
             
         };
     }
-    followUser(id, type){
-        if(type){
-            alert('following user: ' + id)
-        } else {
-            alert('unfollowing user: ' + id)
+    loadUserData(){
+        let {id, login, baseAPI} = this.state;
+        try {
+          fetch(baseAPI + "users/" + id, {
+            headers: {
+                'Authorization': 'Bearer ' + login.access_token,
+            }
+          })
+          .then((response) => response.json())
+          .then((responseData) => {
+              this.setState({profileDataLoaded: true, userData: responseData, is_following: responseData.profile.is_following})
+          })
+          .done();
+          
+        } catch(err) {
+            alert("Error with data -----------" + err);
         }
+      }
+    followUser(id){
+        let { login, baseAPI, is_following } = this.state;
+        fetch(baseAPI + 'social/follow/?id=' + id, {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              "Authorization": "Bearer " + login.access_token,
+              'Content-Type': 'application/json',
+            }
+          })
+          .then(() => this.setState({is_following: !is_following}))
+          .catch();
     }
+
     settings(){
         alert('my settings')
     }
-    componentDidMount() {
+    componentDidMount(){
+        this.loadUserData()
         let { data, login } = this.state
         // Check if is me
         let my_username = login.username
@@ -52,80 +84,88 @@ class Profile extends Component {
         }   
     }
     render() {
-        let { data, myProfile} = this.state
+        let { myProfile, userData, is_following} = this.state
+
         return (
             <View>
-            <View style={{alignItems: 'center', justifyContent: 'center', borderBottomWidth: 2, borderBottomColor: 'black'}}>
-                <Image 
-                    style={styles.profile_pic}
-                    source={{uri: data.profile.profile_picture}}
-                />
-            </View>
-            <View style={{paddingTop: 5, alignItems: 'center',}}>
-                <View style={{flexDirection: 'row'}}>
-                    <Text style={styles.name}> {data.first_name + ' ' + data.last_name}</Text>
-                    {myProfile ? 
-                    <View style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 5}}>
-                        <TouchableOpacity onPress={() => this.settings()}>
-                            <Icon name={'settings'} color={'gray'} size={20}/>
-                        </TouchableOpacity>
+                {userData ? 
+                    <View>
+                        <View style={{alignItems: 'center', justifyContent: 'center', borderBottomWidth: 2, borderBottomColor: 'black'}}>
+                            <Image 
+                                style={styles.profile_pic}
+                                source={{uri: userData.profile.profile_picture}}
+                            />
+                        </View>
+                        <View style={{paddingTop: 5, alignItems: 'center',}}>
+                            <View style={{flexDirection: 'row'}}>
+                                <Text style={styles.name}> {userData.first_name + ' ' + userData.last_name}</Text>
+                                {myProfile ? 
+                                <View style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 5}}>
+                                    <TouchableOpacity onPress={() => this.settings()}>
+                                        <Icon name={'settings'} color={'gray'} size={20}/>
+                                    </TouchableOpacity>
+                                </View>
+                                : 
+                                // Need to do only one, waiting on userData from API
+                                
+                                <View style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 5, flexDirection: 'row'}}>
+                                    {is_following ? 
+                                    <TouchableOpacity onPress={() => this.followUser(userData.id)}>
+                                        <Text style={styles.unFollowButton}>UnFollow</Text>
+                                    </TouchableOpacity>
+                                    : 
+                                    <TouchableOpacity onPress={() => this.followUser(userData.id)}>
+                                        <Text style={styles.followButton}>Follow</Text>
+                                    </TouchableOpacity> 
+                                    }
+                                    
+                                    
+                                </View>
+                                }
+                            </View>
+                            {userData.profile.description &&
+                                <View style={styles.flexInRow}>
+                                    <Icon name='description' /><Text style={styles.userInfo}> {userData.profile.description}</Text>
+                                </View>
+                            }
+                            <View style={styles.flexInRow}>
+                                <Icon name='stars' /><Text style={styles.userInfo}> {'King Beta Sprayer'} </Text>
+                            </View>
+                            <View style={styles.flexInRow}>
+                                <Icon name='event' /><Text style={styles.userInfo}> {userData.date_joined.split('T')[0]} </Text>
+                            </View>
+                            <View style={styles.flexInRow}>
+                                <Tooltip popover={<Text>Followers</Text>}>
+                                    <View style={styles.flexInRow}>
+                                        <Icon name='people' /><Text style={styles.userInfo}> {userData.profile.followers} </Text>
+                                    </View>
+                                </Tooltip>
+                                <Text> </Text>
+                                <Tooltip popover={<Text>Following</Text>}>
+                                    <View style={styles.flexInRow}>
+                                        <Icon name='search' /><Text style={styles.userInfo}> {userData.profile.following} </Text>
+                                    </View>
+                                </Tooltip>
+                            </View>
+                            {userData.profile.location &&
+                                <View style={styles.flexInRow}>
+                                    <Icon name='place' /><Text style={styles.userInfo}> {userData.profile.location}</Text>
+                                </View>
+                            }
+                            
+                            <Tooltip popover={<Text>Total Sends</Text>}>
+                                <View style={styles.flexInRow}>
+                                <Icon name='timeline' /><Text style={styles.userInfo}> {userData.profile.sends} </Text>
+                                </View>
+                            </Tooltip>
+                        </View>
                     </View>
-                    : 
-                    // Need to do only one, waiting on data from API
-                    
-                    <View style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 5, flexDirection: 'row'}}>
-                        {data.profile.is_following ? 
-                        <TouchableOpacity onPress={() => this.followUser(data.id)}>
-                            <Text style={styles.followButton}>Follow</Text>
-                        </TouchableOpacity> 
-                        : 
-                        <TouchableOpacity onPress={() => this.followUser(data.id)}>
-                            <Text style={styles.unFollowButton}>UnFollow</Text>
-                        </TouchableOpacity>
-                        }
-                        
-                        
-                    </View>
-                    }
-                </View>
-                {data.profile.description &&
-                    <View style={styles.flexInRow}>
-                        <Icon name='description' /><Text style={styles.userInfo}> {data.profile.description}</Text>
+                :
+                    <View style={{paddingTop: 20, paddingBottom: 20}}>
+                        <ActivityIndicator size="large" color="#0000ff" />
                     </View>
                 }
-                <View style={styles.flexInRow}>
-                    <Icon name='stars' /><Text style={styles.userInfo}> {'King Beta Sprayer'} </Text>
-                </View>
-                <View style={styles.flexInRow}>
-                    <Icon name='event' /><Text style={styles.userInfo}> {data.date_joined.split('T')[0]} </Text>
-                </View>
-                <View style={styles.flexInRow}>
-                    <Tooltip popover={<Text>Followers</Text>}>
-                        <View style={styles.flexInRow}>
-                            <Icon name='people' /><Text style={styles.userInfo}> {data.profile.followers} </Text>
-                        </View>
-                    </Tooltip>
-                    <Text> </Text>
-                    <Tooltip popover={<Text>Following</Text>}>
-                        <View style={styles.flexInRow}>
-                            <Icon name='search' /><Text style={styles.userInfo}> {data.profile.following} </Text>
-                        </View>
-                    </Tooltip>
-                </View>
-                {data.profile.location &&
-                    <View style={styles.flexInRow}>
-                        <Icon name='place' /><Text style={styles.userInfo}> {data.profile.location}</Text>
-                    </View>
-                }
-                
-                <Tooltip popover={<Text>Total Sends</Text>}>
-                    <View style={styles.flexInRow}>
-                    <Icon name='timeline' /><Text style={styles.userInfo}> {data.profile.sends} </Text>
-                    </View>
-                </Tooltip>
-                
             </View>
-        </View>
         );
     }
 }
