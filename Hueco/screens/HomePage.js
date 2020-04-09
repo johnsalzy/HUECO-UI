96fab5d22f0b051ecbdcf00136573a5f38e18de9
johnsalzy <salzyjohn@gmail.com>
2020-04-08 20:48:25 -0400

    Update HomePage.js

--- Hueco/screens/HomePage.js
+++ Hueco/screens/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View, FlatList, ScrollView } from 'react-native';
+// import { ScrollView } from 'react-native-gesture-handler';
 
 //Import files/componenets
 import SocialMedia from '../components/SocialMedia';
@@ -8,6 +8,8 @@ import Ionicon from '../components/Ionicon';
 import AddOptionModal from '../components/Modals/AddOptions';
 import AddPostModal from '../components/Modals/CreatePost';
 import {app_styles} from '../assets/styles/universal';
+import MediaFilter from '../components/Posts/PostFilter';
+import PostFilter from '../components/Posts/PostFilter';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -19,6 +21,10 @@ export default class HomeScreen extends Component {
         modalAddVisable: false,
         modalAddTick: false,
         modalAddPost: false,
+        recommended_posts: [90, 89, 88, 85, 86, 87, 91, 92, 93, 94],
+        // recommended_posts: [85, 86],
+        refreshingPosts: false,
+
     };
   }
   closeAllModals = () => {
@@ -39,53 +45,56 @@ export default class HomeScreen extends Component {
       modalAddVisable: false, 
       modalAddPost: true,});
   }
+  refreshPosts(){
+    this.setState({refreshingPosts: true})
+
+
+    this.setState({recommended_posts: [90, 89, 88, 84, 85, 86, 87], refreshingPosts: false})
+  }
+  loadMorePosts(){
+    // alert('loading more postst')
+    let { recommended_posts } = this.state
+    // this.setState({recommended_posts: [...recommended_posts, ...[88, 89, 67, 82, 81]] })
+  }
+
   render(){
-    let {modalAddVisable, modalAddPost, modalAddTick} = this.state
+    let {modalAddVisable, modalAddPost, modalAddTick, recommended_posts, refreshingPosts} = this.state
     return (
         <View style={app_styles.screen}>
-          <ScrollView>
-            <View style={{alignItems: 'center'}}>
-              <Text style={styles.textHeader}>New Routes Near You</Text>
-              <View style={styles.headerRow} >
-                <Text> Setter   |    Type      |            Name            | Grade </Text>
-                <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
-                <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
-                <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
-                <Text>  Matt  | Top Rope  | Matt's First Route |   5.9</Text>
+              <View style={{alignItems: 'center'}}>
+                <Text style={styles.textHeader}>New Routes Near You</Text>
+                <View style={styles.headerRow} >
+                  <Text> Setter   |    Type      |            Name            | Grade </Text>
+                  <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
+                  <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
+                  <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
+                  <Text>  Matt  | Top Rope  | Matt's First Route |   5.9</Text>
+                </View>
+              </View>
+              <View style={{alignItems: 'center', height: '100%'}}>
+                  <Text style={styles.textHeader}>Recommended Feed</Text>
+                  <FlatList 
+                    data={recommended_posts}
+                    renderItem={({ item }) => 
+                      <PostFilter id={item}/>
+                    }
+                    onEndReached={() => this.loadMorePosts()}
+                    onEndReachedThreshold={.2}
+                    refreshing={refreshingPosts}
+                    onRefresh={() => this.refreshPosts()}
+                    keyExtractor={item => item}
+                  />
               </View>
 
-              <Text style={styles.textHeader}>Your Send Group Updates</Text>
-              <View style={styles.headerRow} >
-                <Text>John Says: Hello!!</Text>
-                <Text>Matt Says: Just did first v0!!! I am soooo good! (:</Text>
-                <Text>John Climbed: v10</Text>
-                <Text>Matt Climbed: 5.7</Text>
-                <Text>John Says: Hello!!</Text>
-                <Text>Matt Says: Just did first v0!!! I am soooo good! (:</Text>
-                <Text>John Climbed: v10</Text>
-                <Text>Matt Climbed: 5.7</Text>
-                <Text>John Says: Hello!!</Text>
-                <Text>Matt Says: Just did first v0!!! I am soooo good! (:</Text>
-                <Text>John Climbed: v10</Text>
-                <Text>Matt Climbed: 5.7</Text>
-                <Text>John Says: Hello!!</Text>
-                <Text>Matt Says: Just did first v0!!! I am soooo good! (:</Text>
-                <Text>John Climbed: v10</Text>
-                <Text>Matt Climbed: 5.7</Text>
-                <Text>John Says: Hello!!</Text>
-                <Text>Matt Says: Just did first v0!!! I am soooo good! (:</Text>
-                <Text>John Climbed: v10</Text>
-                <Text>Matt Climbed: 5.7</Text>
-                <Text>John Says: Hello!!</Text>
-                <Text>Matt Says: Just did first v0!!! I am soooo good! (:</Text>
-                <Text>John Climbed: v10</Text>
-                <Text>Matt Climbed: 5.7</Text>
-              </View>
+              
 
-              <SocialMedia />
-              <Text>{"\n"}{"\n"}</Text>{/* Breaks needed so social media does not get covered by nav bar */}
-            </View>
-          </ScrollView>
+
+
+
+            {/* <SocialMedia />
+            <Text>{"\n"}{"\n"}</Text>Breaks needed so social media does not get covered by nav bar */}
+
+        {/* Create Post Button */}
           <View style={styles.addButton}>
             <TouchableOpacity onPress={() => this.setState({modalAddVisable: true})}>
                 <Ionicon color={'cornflowerblue'} name={'add-circle'} size={60}/>
