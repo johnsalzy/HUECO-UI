35d1ca66c1be5d3120455522c0caf48d348a05e8
johnsalzy <salzyjohn@gmail.com>
2020-04-09 18:18:04 -0400

    Update Workout.js
    Move recommended routes to workout page

--- Hueco/screens/Workout.js
+++ Hueco/screens/Workout.js
@@ -6,6 +6,7 @@ import {
     TouchableWithoutFeedback,
     Modal,
     Button,
+    StyleSheet
 } from "react-native";
 
 //Import Component/Store/etc.
@@ -19,7 +20,16 @@ class Workout extends Component {
         return (
             <View style={app_styles.screen}>
                 <Text style={{fontSize: 20, fontWeight: 'bold', textAlign: 'center'}}>This is Workouts Page</Text>
-
+                <View style={{alignItems: 'center'}}>
+                    <Text style={styles.textHeader}>New Routes Near You</Text>
+                    <View style={styles.headerRow} >
+                        <Text> Setter   |    Type      |            Name            | Grade </Text>
+                        <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
+                        <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
+                        <Text>  John  | Boulder   | Taco's First Route |   v0</Text>
+                        <Text>  Matt  | Top Rope  | Matt's First Route |   5.9</Text>
+                    </View>
+                </View>
                 <View>
                     <AddTodo />
                     <Text style={{fontSize:20, paddingTop: '5%', paddingLeft: '5%', alignContent: 'center', fontWeight: 'bold'}}>
@@ -32,3 +42,17 @@ class Workout extends Component {
     }
 }
 export default Workout;
+
+const styles = StyleSheet.create({
+    textHeader: {
+      paddingTop: 20, 
+      fontWeight: 'bold', 
+      fontSize: 20
+    },
+    headerRow:{
+      borderWidth: 2,
+      width: '90%',
+      borderColor:'black',
+      margin:10,
+    },
+  });
