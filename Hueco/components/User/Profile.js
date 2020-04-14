99db2c77c52a2ce1327c00461f3c0e9a5a68b87d
johnsalzy <salzyjohn@gmail.com>
2020-04-14 19:16:07 -0400

    Update Profile.js
    Give route info on profile page

--- Hueco/components/User/Profile.js
+++ Hueco/components/User/Profile.js
@@ -10,10 +10,10 @@ import {
     ActivityIndicator,
 } from "react-native";
 import {connect} from 'react-redux';
-import { Tooltip } from 'react-native-elements';
+import { Tooltip, Rating } from 'react-native-elements';
 
 import Icon from '../Ionicon';
-import UserStatView from '../UserStatView';
+import StatView from '../UserStatView';
 import Settings from './Settings/Settings'
 import { fetchGet, fetchPost } from '../../functions/api'
 
@@ -65,11 +65,25 @@ class Profile extends Component {
             let response = await fetchGet('users/' + apiRoute)
             this.setState({data: response})
         } else {
-            let response = await fetchGet('routes/' + propsData + '/')
-            console.log('res', response)
-            let data = {full_name: null, date_joined: null, profile: {profile_picture: null, description: null, sends: 10}}
+            let response = await fetchGet('routes/' + propsData.id + '/')
+            let data = {
+                full_name: null, date_joined: null, 
+                stars: null, setter: null, 
+                rating: null, route_type: null,
+                pitches: null,
+                profile: 
+                    {
+                        profile_picture: null, 
+                        description: null, sends: 10
+                    }
+            }
             data.profile.profile_picture = response.img_url;
             data.full_name = response.name
+            data.rating = response.rating
+            data.stars = response.stars
+            data.setter = response.setter
+            data.route_type = response.route_type
+            data.pitches = response.pitches
             data.date_joined = response.set_date
             this.setState({data})
             //Check if it is the users route
@@ -113,24 +127,40 @@ class Profile extends Component {
                                         </TouchableOpacity>
                                     </View>
                                 : 
-               
-                                
-                                <View style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 5, flexDirection: 'row'}}>
-                                    {data.profile.is_following ? 
-                                    <TouchableOpacity onPress={() => this.followUser(data.id)}>
-                                        <Text style={styles.unFollowButton}>UnFollow</Text>
-                                    </TouchableOpacity>
-                                    : 
-                                    <TouchableOpacity onPress={() => this.followUser(data.id)}>
-                                        <Text style={styles.followButton}>Follow</Text>
-                                    </TouchableOpacity> 
-                                    }
+                                <View>
+                                {type == 'user' &&
+                                    <View style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 5, flexDirection: 'row'}}>
+                                        {data.profile.is_following ? 
+                                        <TouchableOpacity onPress={() => this.followUser(data.id)}>
+                                            <Text style={styles.unFollowButton}>UnFollow</Text>
+                                        </TouchableOpacity>
+                                        : 
+                                        <TouchableOpacity onPress={() => this.followUser(data.id)}>
+                                            <Text style={styles.followButton}>Follow</Text>
+                                        </TouchableOpacity> 
+                                        }
                                     
                                     
+                                    </View>
+                                }
                                 </View>
+                                
                                 }
                             </View>
                             <View style={{alignSelf: 'center', width: '70%', alignItems: 'center'}}>
+                                {type == 'route' &&
+                                    <View style={styles.flexInRow}>
+                                        <Text style={styles.userInfo}>Rating: </Text>
+                                        <Rating
+                                        type='star'
+                                        ratingCount={5}
+                                        imageSize={16}
+                                        readonly
+                                        startingValue={data.stars}
+                                        style={{justifyContent: 'center'}}
+                                        />
+                                    </View>
+                                }
                                 {data.profile.description &&
                                     <View style={{flexDirection: 'row'}}>
                                         <Icon name='description' color={'cornflowerblue'}/>
@@ -171,6 +201,14 @@ class Profile extends Component {
                                         </Tooltip>
                                     </View>
                                 }
+                                {type == 'route' &&
+                                    <View>
+                                        <Text style={styles.userInfo}>Grade: {data.rating}</Text>
+                                        <Text style={styles.userInfo}>Type: {data.route_type}</Text>
+                                        {data.pitches && (data.pitches > 1) && <Text style={styles.userInfo}>Pitches: {data.pitches}</Text>}
+                                        
+                                    </View>
+                                }
                                 {/* Sends count */}
                                 <Tooltip popover={<Text>Total Sends</Text>}>
                                     <View style={styles.flexInRow}>
@@ -179,7 +217,7 @@ class Profile extends Component {
                                 </Tooltip>
                             </View>
                             {/* End of profile view, start of stats view */}
-                            <UserStatView type={type} id={data.id}/>
+                            <StatView type={type} id={data.id}/>
                         </View>
                 :
                     <View style={{paddingTop: 20, paddingBottom: 20}}>
@@ -233,8 +271,6 @@ const styles = StyleSheet.create({
     },
     flexInRow: {
         flexDirection: 'row',
-        paddingTop: '1%',
-        paddingBottom: '1%',
     },
     unFollowButton: {
         color: 'red',
