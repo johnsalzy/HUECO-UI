c2b7ff37ea97bc8216e6b45069624c2232035e15
johnsalzy <salzyjohn@gmail.com>
2020-04-16 15:37:11 -0400

    Update CreateTick.js
    Add rating area, ascent type area, picture, date, and styling

--- Hueco/components/Modals/CreateTick.js
+++ Hueco/components/Modals/CreateTick.js
@@ -7,22 +7,26 @@ import {
     StyleSheet,
     TouchableOpacity,
     Modal,
+    Dimensions,
     Button,
     TextInput,
     Platform,
     ActivityIndicator
 } from "react-native";
-
+import { RadioButton } from 'react-native-paper';
+import { Tooltip, AirbnbRating  } from 'react-native-elements';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { format } from 'date-fns'
+import {connect} from 'react-redux';
 
 //Import Screens/Components/Styles
 import Icon from '../Ionicon';
 import TagRoute from '../Tags/tagRoute';
-
-//Redux imports
-import {connect} from 'react-redux';
+import ImageWithLoader from '../ImageWithLoader';
 
 
 
+const windowWidth = Dimensions.get('window').width;
 const mapStateToProps = state => (
     {
     login: state.login
@@ -40,12 +44,25 @@ class CreateTick extends Component {
             tagRoute: false,
             caption: null,
             response: null,
+            date: Date.now(),
+            showDatePicker: false,
+            checked: 'first',
+            rating: 2.5,
         };
     }
-
-
+    updateDate(date){
+        console.log('date', date)
+        if(date.type == "set"){
+            date = date.nativeEvent.timestamp
+            this.setState({date: date, showDatePicker: false})
+        }
+    }
+    submitTick(){
+        alert('submit tick')
+        //close modal
+    }
     render() {
-        let { tagRoute, data, taggedRoute } = this.state;
+        let { tagRoute, data, taggedRoute, showDatePicker, date, checked, rating } = this.state;
         return (
                 <Modal
                     animationType="fade"
@@ -59,7 +76,95 @@ class CreateTick extends Component {
                             </TouchableOpacity>
                             <View style={{alignItems: 'center',}}>
                                 {data ?
-                                    <Text style={{fontSize: 20}}>{data.name} @ {data.wall}</Text>
+                                    
+
+                                    <View>
+                                        {/* Section to show image/title */}
+                                        <View style={{flexDirection: 'row'}}>
+                                            <View
+                                                style={{
+                                                    width: windowWidth*.35,
+                                                    height: windowWidth*.35,
+                                                    borderRadius: 3,
+                                                    borderColor: 'black',
+                                                    borderWidth: 1,
+                                                    overflow: "hidden"
+                                                }}
+                                            >
+                                                <ImageWithLoader uri={data.picture}/>
+                                            </View>
+                                            <View style={{paddingTop: 10, paddingLeft: 10, paddingRight: 10, width: windowWidth*.6}}>
+                                                <Text style={{fontSize: 30}}>{data.name}</Text>
+                                                <Text style={{fontSize: 20, color: 'darkgray'}}>{data.wall}</Text>
+                                            </View>
+                                        </View>
+
+                                        {/* Section to show date picker */}
+                                        <View>
+                                            <Text style={styles.title}>Date</Text>
+                                            <TouchableOpacity
+                                                onPress={() => this.setState({showDatePicker: true})}
+                                            >
+                                                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                                                    <Icon size={40} color='cornflowerblue' name='today'/>
+                                                    <Text style={{fontSize: 20}}>{format(date, 'MM/dd/yyyy')}</Text>
+                                                </View>
+                                            </TouchableOpacity>
+                                            {showDatePicker && 
+                                                <DateTimePicker
+                                                    value={date}
+                                                    mode={"date"}
+                                                    display="default"
+                                                    onChange={(date) => this.updateDate(date)}
+                                                    maximumDate={Date.now()}
+                                                />
+                                            }
+                                        </View>
+                                        
+                                        {/* Section to mark ascent type */}
+                                        <View>
+                                            <Text style={styles.title}>Ascent Type</Text>
+                                            <View style={styles.radioButton}>
+                                                <RadioButton
+                                                    value="first"
+                                                    status={checked === 'first' ? 'checked' : 'unchecked'}
+                                                    onPress={() => { this.setState({ checked: 'first' }); }}
+                                                />
+                                                <Text>On Sight</Text>
+                                            </View>
+                                            <View style={styles.radioButton}>
+                                                <RadioButton
+                                                    value="second"
+                                                    status={checked === 'second' ? 'checked' : 'unchecked'}
+                                                    onPress={() => { this.setState({ checked: 'second' }); }}
+                                                />
+                                                <Text>Flash</Text>
+                                            </View>
+                                            <View style={styles.radioButton}>
+                                                <RadioButton
+                                                    value="third"
+                                                    status={checked === 'third' ? 'checked' : 'unchecked'}
+                                                    onPress={() => { this.setState({ checked: 'third' }); }}
+                                                />
+                                                <Text>Fell / Hung</Text>
+                                            </View>
+                                        </View>
+                                        {/* Section to make rating */}
+                                        <View>
+                                            <Text style={styles.title}>Rating</Text>
+                                            <View style={{alignItems: 'flex-start', marginTop: -50}}>
+                                                <AirbnbRating 
+                                                    type='star'
+                                                    count={5}
+                                                    size={25}
+                                                    reviews={[]}
+                                                    defaultRating={rating}
+                                                    onFinishRating={(rating) => this.setState({rating: rating})}
+                                                />
+                                            </View>
+                                        </View>
+                                    
+                                    </View>
                                 :
                                     <View>
                                         <Text style={styles.text}>Tag A Route</Text>
@@ -77,11 +182,15 @@ class CreateTick extends Component {
                                         }
                                     </View>
                                 }
-
-
                                 
-                                
-                                
+                                <TouchableOpacity
+                                    style={{marginTop: 15}}
+                                    onPress={() => this.submitTick()}
+                                >
+                                    <Text style={styles.add}>
+                                        Submit
+                                    </Text>
+                                </TouchableOpacity>
                             </View>
                         </ScrollView>
                     </View>
@@ -110,11 +219,33 @@ const styles = StyleSheet.create({
     flexRow: {
         flexDirection: 'row',
     },
+    radioButton: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
     text_input: {
         borderWidth: 2,
         backgroundColor: 'lightskyblue',
         borderColor: 'black',
         borderRadius: 4,
         paddingLeft: 10,
+    },
+    add: {
+        color: 'white',
+        padding: 5,
+        backgroundColor: 'dodgerblue',
+        borderRadius: 5,
+        fontWeight: 'bold',
+        fontSize: 20,
+        textAlignVertical: 'center',
+        textAlign: 'center',
+        height: 50,
+        width: 200
+    },
+    title: {
+        marginTop: 5,
+        fontSize: 20,
+        fontWeight: "bold",
+
     }
 });
