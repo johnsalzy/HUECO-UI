c5a409bcfba16ea1ea604e5e5101962fcbb8795b
johnsalzy <salzyjohn@gmail.com>
2020-04-21 18:09:02 -0400

    Refactor components

---
+++ Hueco/components/Routes/Routes.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+// Import
+import {app_styles} from '../../assets/styles/universal'
+import RouteResult from '../Routes/RouteResult';
+
+export default class RouteView extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: this.props.data,
+        };
+
+    }
+    render(){
+        let { data } = this.state
+        return (
+            <View style={app_styles.background}>
+                {data.count > 0 ?
+                  data.results.map((data, index) => (
+                        <RouteResult key={index} data={data}/>
+                  ))
+                : 
+                  <Text>No Routes Found :(</Text> 
+                }
+            </View>
+        );
+    }
+}
--- Hueco/components/Search/Wrap.js
+++ Hueco/components/Search/Wrap.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 // Import Components
 import UserView from './Users';
 import AreaView from '../Areas/Area';
-import RouteView from './Routes';
+import RouteView from '../Routes/Routes';
 import WallView from '../Walls/Walls';
 
 
@@ -24,10 +24,10 @@ class SearchScreen extends React.Component {
         let {category, data} = this.state
         return (
             <View style={{paddingTop: '2%'}}>
-                {category == 'Users' ? <UserView data={data}/>: null}
-                {category == 'Areas' ? <AreaView data={data}/>: null}
-                {category == 'Routes' ? <RouteView data={data}/>: null}
-                {category == 'Walls' ? <WallView data={data}/>: null}
+                {category == 'Users' && <UserView data={data}/>}
+                {category == 'Areas' && <AreaView data={data}/>}
+                {category == 'Routes' && <RouteView data={data}/>}
+                {category == 'Walls' && <WallView data={data}/>}
             </View>
         );
     }
