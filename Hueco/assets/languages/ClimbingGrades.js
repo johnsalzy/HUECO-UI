df969e1b65d01526b62ab4ab47056ec691f703a2
johnsalzy <salzyjohn@gmail.com>
2020-05-02 22:35:47 -0400

    Create ClimbingGrades.js
    Created page to see import climbing grades

---
+++ Hueco/assets/languages/ClimbingGrades.js
@@ -0,0 +1,18 @@
+const boulder_grade = {
+    v_scale: [{name: "v0"}, {name: "v1"}, {name: "v2"}, {name: "v3"}, {name: "v4"}, {name: "v5"}, {name: "v6"}, {name: "v7"}, {name: "v8"}, {name: "v9"}, {name: "v10"}, {name: "v11"}, {name: "v12"}, {name: "v13"}, {name: "v14"}, {name: "v15"}, {name: "v16"}, {name: "v17"}, ],
+}
+
+const rope_grade = {
+    yosemite_D_S: [
+        {name: "5.0"}, {name: "5.1"}, {name: "5.2"}, {name: "5.3"}, {name: "5.4"}, 
+        {name: "5.5"}, {name: "5.6"}, {name: "5.7"}, {name: "5.8"}, {name: "5.9"}, 
+        {name: "5.10a"}, {name: "5.10b"}, {name: "5.10c"}, {name: "5.10d"}, 
+        {name: "5.11a"}, {name: "5.11b"}, {name: "5.11c"}, {name: "5.11d"}, 
+        {name: "5.12a"}, {name: "5.12b"}, {name: "5.12c"}, {name: "5.12d"}, 
+        {name: "5.13a"}, {name: "5.13b"}, {name: "5.13c"}, {name: "5.13d"}, 
+        {name: "5.14a"}, {name: "5.14b"}, {name: "5.14c"}, {name: "5.14d"}, 
+        {name: "5.15a"}, {name: "5.15b"}, {name: "5.15c"}, {name: "5.15d"}, 
+    ],
+}
+
+export { boulder_grade, rope_grade}
