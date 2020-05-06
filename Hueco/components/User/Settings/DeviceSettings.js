d93a9f114994120f23cd6d6a161c6532378bb16b
johnsalzy <salzyjohn@gmail.com>
2020-05-06 19:24:31 -0400

    Create DeviceSettings.js

---
+++ Hueco/components/User/Settings/DeviceSettings.js
@@ -0,0 +1,90 @@
+// Import needed Libs
+import React, { Component } from "react";
+import {
+    View,
+    ScrollView,
+    Modal,
+    Text,
+    TouchableOpacity,
+    Switch,
+} from "react-native";
+import {connect} from 'react-redux';
+
+import { titles } from '../../../assets/styles/text'
+import { registerForPushNotificationsAsync } from '../../Notifications/getPermission';
+const mapStateToProps = state => (
+    {
+      login: state.login,
+    }
+)
+
+class DeviceSettings extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            profile_private: false,
+            notifications_enabled: false,
+            notifications_sound: false,
+        };
+    }
+    async enableNotifications(){
+        response = await registerForPushNotificationsAsync()
+        if(response.expoPushToken != ""){ this.setState({expoPushToken: response.expoPushToken})}
+        else{this.setState({notifications_enabled: false})}
+    }
+
+
+
+    render() {
+        let {profile_private, notifications_enabled, notifications_sound} = this.state;
+        return (
+            <ScrollView style={{width: '100%', height: '100%', }}>
+                <View style={{ alignSelf: 'center', width: '80%'}}>
+                    <Text style={titles.page_header}>User Settings</Text>
+
+                    <View style={{paddingVertical: 10}}>
+                        <Text style={titles.titleDivider}>Profile</Text>
+                        <View style={{flexDirection: 'row', alignItems: 'center', paddingLeft: 5}}>
+                            <Text style={{width: '40%'}}>Private</Text>
+                            <Switch
+                                style = {{width: '60%'}}
+                                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                                thumbColor={profile_private ? "#2239e3" : "#f4f3f4"}
+                                ios_backgroundColor="#3e3e3e"
+                                onValueChange={() => this.setState({profile_private: !profile_private})}
+                                value={profile_private}
+                            />
+                        </View>
+                    </View>
+
+                    <View style={{paddingVertical: 10}}>
+                        <Text style={titles.titleDivider}>Notifications</Text>
+                        <View style={{flexDirection: 'row', alignItems: 'center', paddingLeft: 5}}>
+                            <Text style={{width: '40%'}}>Enabled</Text>
+                            <Switch
+                                style = {{width: '60%'}}
+                                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                                thumbColor={notifications_enabled ? "#2239e3" : "#f4f3f4"}
+                                ios_backgroundColor="#3e3e3e"
+                                onValueChange={() => this.setState({notifications_enabled: !notifications_enabled})}
+                                value={notifications_enabled}
+                            />
+                        </View>
+                        <View style={{flexDirection: 'row', alignItems: 'center', paddingLeft: 5}}>
+                            <Text style={{width: '40%'}}>Sound</Text>
+                            <Switch
+                                style = {{width: '60%'}}
+                                trackColor={{ false: "#767577", true: "#81b0ff" }}
+                                thumbColor={notifications_sound ? "#2239e3" : "#f4f3f4"}
+                                ios_backgroundColor="#3e3e3e"
+                                onValueChange={() => this.setState({notifications_sound: !notifications_sound})}
+                                value={notifications_sound}
+                            />
+                        </View>
+                    </View>
+                </View>
+            </ScrollView>
+        );
+    }
+}
+export default connect(mapStateToProps)(DeviceSettings);
