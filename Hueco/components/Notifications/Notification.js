473590f7b07b107769ba906da4dee9317d156377
johnsalzy <salzyjohn@gmail.com>
2020-05-06 21:58:02 -0400

    Update Notification.js
    Notifications can now show posts, users, routes

--- Hueco/components/Notifications/Notification.js
+++ Hueco/components/Notifications/Notification.js
@@ -1,51 +1,22 @@
 import React from 'react';
 import { Text, View, Button, Vibration, Platform } from 'react-native';
 import { Notifications } from 'expo';
-import * as Permissions from 'expo-permissions';
-import Constants from 'expo-constants';
 
 import { registerForPushNotificationsAsync } from './getPermission';
-
+import UserView from '../Modals/ModalView';
+import PostView from '../Modals/ViewPost';
 export default class Notification extends React.Component {
   state = {
     expoPushToken: '',
     notification: {},
-  };
-
-  registerForPushNotificationsAsync = async () => {
-    if (Constants.isDevice) {
-      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-      let finalStatus = existingStatus;
-      console.log( finalStatus)
-      if (existingStatus !== 'granted') {
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-      token = await Notifications.getExpoPushTokenAsync();
-      console.log(token);
-      this.setState({ expoPushToken: token });
-    } else {
-      alert('Must use physical device for Push Notifications');
-    }
-
-    if (Platform.OS === 'android') {
-      Notifications.createChannelAndroidAsync('default', {
-        name: 'default',
-        sound: true,
-        priority: 'max',
-        vibrate: [0, 250, 250, 250],
-      });
-    }
+    modalRouteVisable: false,
+    modalUserVisable: false,
+    modalPostVisable: false,
   };
 
   async componentDidMount() {
-    response = await registerForPushNotificationsAsync();
-    console.log('response', response)
-    if(response.expoPushToken != ""){ this.setState({expoPushToken: response.expoPushToken})}
+    let response = await registerForPushNotificationsAsync();
+    if(response.expoPushToken){ this.setState({expoPushToken: response.expoPushToken})}
     // Handle notifications that are received or selected while the app
     // is open. If the app was closed and then opened by tapping the
     // notification (rather than just tapping the app icon to open it),
@@ -56,44 +27,20 @@ export default class Notification extends React.Component {
 
   _handleNotification = notification => {
     Vibration.vibrate();
-    console.log(notification);
-    this.setState({ notification: notification });
-  };
-
-  // Can use this function below, OR use Expo's Push Notification Tool-> https://expo.io/dashboard/notifications
-  sendPushNotification = async () => {
-    const message = {
-      to: this.state.expoPushToken,
-      sound: 'default',
-      title: 'Original Title',
-      body: 'And here is the body!',
-      data: { data: 'goes here' },
-      _displayInForeground: true,
-    };
-    const response = await fetch('https://exp.host/--/api/v2/push/send', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Accept-encoding': 'gzip, deflate',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(message),
-    });
+    if(notification.origin == "selected"){
+      if(notification.data.type == "post"){this.setState({modalPostVisable: true, notification: notification})}
+      else if(notification.data.type == "route"){this.setState({modalRouteVisable: true, notification: notification})}
+      else if(notification.data.type == "user"){this.setState({modalUserVisable: true, notification: notification})}
+    }
   };
 
   render() {
+    let { modalUserVisable, modalPostVisable, modalRouteVisable, notification } = this.state;
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'space-around',
-        }}>
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Origin: {this.state.notification.origin}</Text>
-          <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
-        </View>
-        <Button title={'Press to Send Notification'} onPress={() => this.sendPushNotification()} />
+      <View>
+        {modalUserVisable && <UserView data={ {id: notification.data.id}} type={'user'} closeModal={() => this.setState({modalUserVisable: false})}/>}
+        {modalRouteVisable && <UserView data={ {id: notification.data.id}} type={'route'} closeModal={() => this.setState({modalRouteVisable: false})}/>}
+        {modalPostVisable && <PostView post_id={notification.data.id} closeModal={() => this.setState({modalPostVisable: false})}/>}
       </View>
     );
   }
